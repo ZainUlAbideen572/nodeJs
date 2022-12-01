e6797564d4ca7528400d069c3621ea88e2830d54
ZainUlAbideen572 <117797040+ZainUlAbideen572@users.noreply.github.com>
2022-12-01 15:30:51 +0530

    Delete indiamodel.js

--- models/indiamodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const schema=new mongoose.Schema({
-    Name:String,
-    Language:String,
-    Chiefminister:String
-
-})
-const indiamodel= new mongoose.model('state',schema)
-module.exports=indiamodel;
